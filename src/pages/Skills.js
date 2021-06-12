1f96d1238521f721f77adb00d286f52f1439030e
Seth Torek <seth.torek@gmail.com>
2021-06-12 18:45:10 -0400

    added and styling skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,10 +1,37 @@
 import React from "react";
-// import Picture from "../images/profilePic.jpg";
 
 function Skills() {
   return (
     <div>
-        Skills section!!!!!!!!!!!
+      <div className="skill-slant">
+        <div className="skill-title">
+          <p>Skills</p>
+        </div>
+      </div>
+      <div className="badge-cont">
+        <i class="badge fab fa-js-square fa-4x"></i>
+        <i class="badge fab fa-html5 fa-4x"></i>
+        <i class="badge fab fa-css3 fa-4x"></i>
+        <i class="badge fab fa-react fa-4x"></i>
+        <i class="badge fas fa-database fa-4x"></i>
+        <i class="badge fab fa-node fa-4x"></i>
+        <i class="badge fas fa-flask fa-4x"></i>
+        <i class="badge fab fa-git fa-4x"></i>
+      </div>
+      <div className="skill-cont">
+        <p className="elegantshadow">JavaScript</p>
+        <p className="elegantshadow">jQuery</p>
+        <p className="elegantshadow">HTML5</p>
+        <p className="elegantshadow">CSS</p>
+        <p className="elegantshadow">MERN</p>
+        <p className="elegantshadow">MySQL</p>
+        <p className="elegantshadow">REST APIs</p>
+        <p className="elegantshadow">Bootstrap</p>
+        <p className="elegantshadow">Unit Testing</p>
+        <p className="elegantshadow">Responsive Design</p>
+        <p className="elegantshadow">Version Control / Git</p>
+        <p className="elegantshadow">Agile Work Environments</p>
+      </div>
     </div>
   );
 }
