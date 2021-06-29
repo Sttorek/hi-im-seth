fd0e8abd99639d477f3ad4653a7f127e536075f8
Seth Torek <seth.torek@gmail.com>
2021-06-29 18:43:49 -0400

    added pic and title to work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,18 @@
 import React from "react";
-// import Picture from "../images/profilePic.jpg";
+import Picture from "../images/profilePic.jpg";
 
 function Work() {
   return (
     <div>
+      <div className="workHead">
+        <div className="workTitle">
+          <p>Check Out My Work</p>
+        </div>
+        <div id="circle">
+            <img id="profile-pic" alt="profile pic" src={Picture} />
+        </div>
+      </div>
+
         <div className="firstProjectBackground"></div>
         <div className="secondProjectBackground"></div>
         <div className="thirdProjectBackground"></div>
