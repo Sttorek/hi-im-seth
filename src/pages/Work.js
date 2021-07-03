aed42e4762f0f23f5526f0d83328569371420a43
Seth Torek <seth.torek@gmail.com>
2021-07-03 18:31:54 -0400

    layed out project page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Picture from "../images/profilePic.jpg";
+import Project1 from "../images/project1.png";
+import Project2 from "../images/project2.png";
+import Project3 from "../images/project3.png";
+import Project4 from "../images/project4.png";
 
 function Work() {
   return (
@@ -14,16 +18,45 @@ function Work() {
       </div>
 
         <div className="firstProjectBackground">
-          <div className="projCont"></div>
+          <div className="projCont">
+            <div className="Titles">
+              <p className="projectTitles">Git Commit</p> 
+              <p>An app designed for developers to practice their skills, tract their progress and learn new skills</p>
+            </div>
+              <img className="projImg" alt="project" src={Project1} />
+          </div>
         </div>
+
         <div className="secondProjectBackground">
-        <div className="projCont"></div>
+          <div className="projCont">
+            <div className="Titles">
+              <p className="projectTitles">Home Route</p>
+              <p>An app designed for developers to practice their skills, tract their progress and learn new skills</p>
+            </div>
+              <img className="projImg" alt="project" src={Project2} />
+          </div>
         </div>
+
+
         <div className="thirdProjectBackground">
-        <div className="projCont"></div>
+          <div className="projCont">
+            <div className="Titles">
+             <p className="projectTitles">Snoopie</p>
+             <p>An app designed for developers to practice their skills, tract their progress and learn new skills</p>
+            </div>
+              <img className="projImg" alt="project" src={Project3} />
+          </div>
         </div>
+
+
         <div className="fourthProjectBackground">
-        <div className="projCont"></div>
+          <div className="projCont">
+            <div className="Titles">
+              <p className="projectTitles">Work Day <br /> Scheduler</p>
+              <p>An app designed for developers to practice their skills, tract their progress and learn new skills</p>
+            </div>
+              <img className="projImg" alt="project" src={Project4} />
+          </div>
         </div>
 
     </div>
