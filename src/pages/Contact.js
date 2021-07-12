d9fc71298aae763d819cf5d73bdb390e17fcb34b
Seth Torek <seth.torek@gmail.com>
2021-07-12 12:26:37 -0400

    added contact email section and functionality

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,46 @@
 import React from "react";
 // import Picture from "../images/profilePic.jpg";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_1ns51q9",
+        "template_pjuscog",
+        e.target,
+        "user_wr8XmGykrVR7pFkzOtLNc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <div>
-        Contact section!!!!!!!!!!!
+      <div className="contactBox">
+        <h1 className="contactTitle">Let's Talk</h1>
+
+        <form className="contact-form" onSubmit={sendEmail}>
+          <label>Subject</label>
+          <input type="text" name="subject" />
+          <label>Name</label>
+          <input type="text" name="name" />
+          <label>Email</label>
+          <input type="email" name="email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
 }
